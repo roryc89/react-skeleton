141b525d8a3269dd431a2649dd97dc30b861ee2c
Rory Campbell <rorycampbell@rorys-mbp.home>
2016-02-13 14:37:30 +0000

    builds react skeleton

---
+++ src/components/List.jsx
@@ -0,0 +1,34 @@
+var React = require('react');
+var ListItem = require('./ListItem.jsx');
+
+var ingredients = [
+  {
+    id: 1,
+    text: 'cheese'
+  },
+  {
+    id:2,
+    text: 'ham'
+  },
+  {
+    id:3,
+    text: 'potatoes'
+  }
+];
+
+var List = React.createClass({
+
+  render:function(){
+
+    var ListItems = ingredients.map(function(item){
+
+      return <ListItem key={item.id} ingredient = {item.text} />;
+
+    });
+
+    return (<ul>{ListItems}</ul>)
+  }
+})
+
+
+module.exports = List;
